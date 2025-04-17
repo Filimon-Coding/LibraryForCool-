cb751165476d3cb4dcc47d95de1a36d1c0e9ddd3
Filimon-Coding <filimon.nuguse22@gmail.com>
2025-04-17 19:36:25 +0200

    first ever commit

---
+++ src/main/java/com/example/eksam2024minv/Eksam2024MinVApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksam2024minv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksam2024MinVApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eksam2024MinVApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eksam2024minv/Eksam2024MinVApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eksam2024minv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eksam2024MinVApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
